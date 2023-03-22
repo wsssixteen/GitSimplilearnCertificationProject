4aeab655717c88bfb9c2fb1d7a66d08dc9ee2254
wsssixteen <vice44wsssixteen@gmail.com>
2023-03-22 14:49:22 +0800

    iBrF1: Added name.

--- project.java
+++ project.java
@@ -1,8 +1,10 @@
 class Project {
     public static void main(String[] args) {
+        String myName = "Ahmad Ridhwan Bin Anuar";
         String courseName = "Git";
         String trainingProvider= "Simplilearn";
 
+        System.out.println(myName);
         System.out.println("This is for certification for the course " + courseName + " provided by " + trainingProvider);
     }
 }

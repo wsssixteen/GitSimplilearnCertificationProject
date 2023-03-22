80093e948579dcd14d9f5319bfd44297a67ba369
wsssixteen <vice44wsssixteen@gmail.com>
2023-03-22 14:27:20 +0800

    Main: Use thread-safe string.

--- project.java
+++ project.java
@@ -1,8 +1,11 @@
 class Project {
     public static void main(String[] args) {
-        String courseName = "Git";
-        String trainingProvider= "Simplilearn";
+        StringBuffer courseName = new StringBuffer("Git Training");
+        StringBuffer trainingProvider = new StringBuffer("Simplilearn");
 
-        System.out.println("This is for certification for the course " + courseName + " provided by " + trainingProvider);
+        System.out.println("This project is created for the certification for the course " 
+        + courseName.toString() 
+        + " provided by " 
+        + trainingProvider.toString());
     }
 }

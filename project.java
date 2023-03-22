390e154f73012e048cdc33f0e47061f80cab3691
wsssixteen <vice44wsssixteen@gmail.com>
2023-03-22 13:02:18 +0800

    Main: Added project.java

---
+++ project.java
@@ -0,0 +1,8 @@
+class Project {
+    public static void main(String[] args) {
+        String courseName = "Git";
+        String trainingProvider= "Simplilearn";
+
+        System.out.println("This is for certification for the course " + courseName + " provided by " + trainingProvider);
+    }
+}

54a0904c12a44a210a7ebcd4893a6b4814d128f8
wsssixteen <vice44wsssixteen@gmail.com>
2023-03-22 14:53:58 +0800

    iBrF1: Added CEO name.

--- project.java
+++ project.java
@@ -1,10 +1,12 @@
 class Project {
     public static void main(String[] args) {
-        String myName = "Ahmad Ridhwan Bin Anuar";
+        StringBuffer myName = new StringBuffer("Ahmad Ridhwan Bin Anuar");
         String courseName = "Git";
         String trainingProvider= "Simplilearn";
+        StringBuffer simplilearnCEO = new StringBuffer("Krishna Kumar");
 
-        System.out.println(myName);
+        System.out.println(myName.toString());
         System.out.println("This is for certification for the course " + courseName + " provided by " + trainingProvider);
+        System.out.println("Signed by the CEO of Simplilearn: " + simplilearnCEO.toString());
     }
 }

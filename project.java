ca2d50ea70db3acb5db754d41857b585bdf2981b
wsssixteen <vice44wsssixteen@gmail.com>
2023-03-22 15:11:58 +0800

    iBrF1: Added certification code.

--- project.java
+++ project.java
@@ -4,9 +4,11 @@ class Project {
         String courseName = "Git";
         String trainingProvider= "Simplilearn";
         StringBuffer simplilearnCEO = new StringBuffer("Krishna Kumar");
+        int certificationCode = 4234726; 
 
         System.out.println(myName.toString());
         System.out.println("This is for certification for the course " + courseName + " provided by " + trainingProvider);
+        System.out.println("Certification code: " + certificationCode);
         System.out.println("Signed by the CEO of Simplilearn: " + simplilearnCEO.toString());
     }
 }
